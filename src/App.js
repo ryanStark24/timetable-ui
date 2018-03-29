b2a32ec028cb77b806e98f1dbf88cda5a4928e83
ryanstark24 <anshul2411.rs@gmail.com>
2018-03-29 18:01:25 +0530

    router linking updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Homepage from './components/Homepage';
-const App = () => (<Router>
-  <Route exact path='/' component={Homepage}/>
+const App = () => (<Router basename={process.env.PUBLIC_URL} >
+  <Route exact url="/"component={Homepage}/>
 </Router>);
 
 export default App;
